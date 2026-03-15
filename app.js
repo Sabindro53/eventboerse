44a68f94181ef38991a4732ced39c079c1ecabd2
Sabindro53 <Sandro.Salvaggio@icloud.com>
2026-03-15 11:00:51 +0100

    v87: Detail-Galerie Bilder mit Animationen (staggered reveal + hover zoom)

--- app.js
+++ app.js
@@ -6163,7 +6163,7 @@ function initCookieConsent() {
 }
 
 // ========== UPDATE NOTIFICATION ==========
-var _EB_VERSION = '86';
+var _EB_VERSION = '87';
 function showUpdateNotification() {
   var lastVersion = localStorage.getItem('eb_last_version');
   if (lastVersion === _EB_VERSION) return;
